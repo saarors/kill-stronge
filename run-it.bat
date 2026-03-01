9031c17da3cf911eb8b0560631b36238cda91e3e
IWO <saarleshembar@gmail.com>
2026-03-01 16:22:11 +0200

    Delete run-it.bat

--- run-it.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-start "" powershell -WindowStyle Hidden -Command "Get-Content 'domains.txt' | ForEach-Object { Start-Process curl -ArgumentList '-s -o nul -IL ' + $_ -WindowStyle Hidden }"
-exit
